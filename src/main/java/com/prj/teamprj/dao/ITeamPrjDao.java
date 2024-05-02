d40de81e876c81382834aefc167655e4d7c12c13
Park Saehee <saehee0427@gmail.com>
2024-05-02 11:18:28 +0900

    Interface - ITeamPrjDao.java 생성 mapper - teamrpj-mapper.xml 생성
    기본 기능 (insert / select / delete / detail) 틀 구현

---
+++ src/main/java/com/prj/teamprj/dao/ITeamPrjDao.java
@@ -0,0 +1,21 @@
+package com.prj.teamprj.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.prj.teamprj.dto.MyPageDto;
+
+@Mapper
+public interface ITeamPrjDao {
+	
+	public List<MyPageDto> getList();
+
+	public void write(@Param("dto") MyPageDto dto);
+
+	public MyPageDto getDto(@Param("bno") String bno);
+
+	public void delete(@Param("bno") String bno);
+	
+}
